3d2e4936544bc4d4a21c4fc2a9e4aeaf23dd690c
Bhuvan Karuturi <bhuvankaruturi123@gmail.com>
2020-01-27 19:34:56 -0600

    added user email auth

---
+++ socialapp-functions/dbschema.js
@@ -0,0 +1,13 @@
+// Only for reference. Not to be used in actual code
+
+const db = {
+    posts: [
+        {
+            userHandle: 'user',
+            body: 'post content',
+            createdAt: '2020-01-28T01:10:01.056Z',
+            likes: 2,
+            commentCount: 2
+        }
+    ]
+}
--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -10,12 +10,32 @@ admin.initializeApp({
 const express = require('express');
 const app = express();
 
+const firebase = require('firebase');
+let firebaseConfig = {
+    apiKey: "AIzaSyCNTLnPuN3e9kdHlFApSBeMlBb-BDyz0fI",
+    authDomain: "socialapp-5380a.firebaseapp.com",
+    databaseURL: "https://socialapp-5380a.firebaseio.com",
+    projectId: "socialapp-5380a",
+    storageBucket: "socialapp-5380a.appspot.com",
+    messagingSenderId: "928558151070",
+    appId: "1:928558151070:web:25a90a306e1f650edf528f",
+    measurementId: "G-JTTGB8T8P0"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 // get posts
 app.get('/posts', (request, response) => {
-    admin.firestore().collection("posts").get().then((querySnapshot) => {
+    admin.firestore()
+    .collection("posts")
+    .orderBy('createdAt', 'desc')
+    .get().then((querySnapshot) => {
         let posts = [];
         querySnapshot.forEach((doc) => {
-            posts.push(doc.data());
+            posts.push({
+                postId: doc.id,
+                ...doc.data()
+            });
         });
         return response.json(posts);
     }).catch((error) => console.error(error));
@@ -29,7 +49,7 @@ app.post('/post', (request, response) => {
     let newPost = {
         body:  request.body.body,
         userHandle: request.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin.firestore()
@@ -41,6 +61,32 @@ app.post('/post', (request, response) => {
     .catch((error) => {
         response.status(500).json({error : 'something went wrong'});
         console.error(error);
+    });
+});
+
+// user signup
+app.post('/signup', (request, response) => {
+    let newUser = {
+        email: request.body.email,
+        password: request.body.password,
+        confirmPassword: request.body.confirmPassword,
+        userHandle: request.body.userHandle
+    };
+
+    //TODO : user details validation
+
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+        return response
+                .status(201)
+                .json({message: `user {data.user.uid} has been successfully created`});
+    })
+    .catch((error) => {
+        return response
+                .status(500)
+                .json({error: error.code});
     });
 });
 
